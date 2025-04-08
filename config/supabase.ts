1ba890357ab7cc8384208450dd684a7bfa18bf5c
effy <eliftaydin@gmail.com>
2025-04-08 20:37:39 +0200

    -

--- config/supabase.ts
+++ config/supabase.ts
@@ -1,7 +1,14 @@
+// config/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
 
 const supabaseUrl = Constants.expoConfig?.extra?.SUPABASE_URL;
 const supabaseAnonKey = Constants.expoConfig?.extra?.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL og Anon Key mangler!');
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
