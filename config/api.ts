ee200e9b3fdaddd93eb97e8ebd9a04eb548ed8fc
effy <eliftaydin@gmail.com>
2025-04-12 16:56:07 +0200

    Strapi

--- config/api.ts
+++ config/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://aldra-app.onrender.com'; // ← Aldra-backend (Render)
+export const API_URL = 'https://aldra-app.onrender.com'; // ← Aldra-backend 
 export const STRAPI_URL = 'https://aldra-cms.onrender.com/api'; // ← Strapi
