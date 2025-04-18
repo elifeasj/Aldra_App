c8f98061be51954112b72ef6a9df5ceec87db42d
effy <eliftaydin@gmail.com>
2025-04-18 18:35:07 +0200

    Update guideUtils.ts

--- utils/guideUtils.ts
+++ utils/guideUtils.ts
@@ -1,19 +1,21 @@
 import { Guide } from '../types/guides';
 
 export const mapGuideData = (guide: any): Guide => {
-  console.log('🧪 Hele guide:', JSON.stringify(guide, null, 2)); // <- HER!
   const attributes = guide.attributes || {};
   const imageUrl = attributes.image?.data?.attributes?.url;
 
   return {
     id: guide.id,
-    title: guide.title || 'Uden titel',
-    category: guide.category || 'Ukategoriseret',
-    image: guide.image || 'https://aldra-cms.up.railway.app/uploads/image2.png',
-    tags: guide.tags || [],
-    relation: guide.relation || '',
-    help_tags: guide.help_tags || [],
-    visible: guide.visible ?? true,
-    content: guide.content || '',
+    title: attributes.title || 'Uden titel',
+    category: attributes.category?.data?.attributes?.title || 'Ukategoriseret',
+    image: imageUrl
+      ? `https://aldra-cms.up.railway.app${imageUrl}`
+      : 'https://aldra-cms.up.railway.app/uploads/image2.png',
+    tags: attributes.tags || [],
+    relation: attributes.relation || '',
+    help_tags: attributes.help_tags || [],
+    visible: attributes.visible ?? true,
+    content: attributes.content || '',
   };
 };
+
