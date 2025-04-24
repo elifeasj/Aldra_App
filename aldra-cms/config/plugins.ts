2f76271c297a6a724f8b09b59e57167e47fd3c78
effy <eliftaydin@gmail.com>
2025-04-24 11:53:40 +0200

    fix

--- aldra-cms/config/plugins.ts
+++ aldra-cms/config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: '@strapi/provider-upload-aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+          endpoint: env('AWS_ENDPOINT'),
+          s3ForcePathStyle: env.bool('AWS_S3_FORCE_PATH_STYLE', true),
+        },
+      },
+    },
+  });
+  
