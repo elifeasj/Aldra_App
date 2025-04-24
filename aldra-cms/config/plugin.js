ef213d24a91ab301a8fc4774130e8d57abedf3bd
effy <eliftaydin@gmail.com>
2025-04-24 21:00:42 +0200

    -

--- aldra-cms/config/plugin.js
+++ aldra-cms/config/plugin.js
@@ -1,23 +1,12 @@
-module.exports = ({ env }) => {
-    console.log('S3 Bucket:', env('AWS_BUCKET'));
-    console.log('S3 Endpoint:', env('AWS_ENDPOINT'));
-    return {
-      upload: {
-        config: {
-          provider: '@strapi/provider-upload-aws-s3',
-          providerOptions: {
-            s3Options: {
-              accessKeyId: env('AWS_ACCESS_KEY_ID'),
-              secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-              region: env('AWS_REGION'),
-              endpoint: env('AWS_ENDPOINT'),
-              s3ForcePathStyle: env.bool('AWS_S3_FORCE_PATH_STYLE', true),
-            },
-            params: {
-              Bucket: 'strapi-media',
-            },
-          },
-        },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-supabase',
+      providerOptions: {
+        apiUrl: env('SUPABASE_URL'),
+        apiKey: env('SUPABASE_SERVICE_ROLE'),
+        bucket: 'strapi-media',
       },
-    };
-  };
+    },
+  },
+});
