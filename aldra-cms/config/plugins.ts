a0e7c9e20fe7be7234e82578547c3ce51e2bd3d2
effy <eliftaydin@gmail.com>
2025-04-24 12:41:56 +0200

    Update plugins.ts

--- aldra-cms/config/plugins.ts
+++ aldra-cms/config/plugins.ts
@@ -1,19 +1,19 @@
 export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: '@strapi/provider-upload-aws-s3',
-      providerOptions: {
-        s3Options: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-          region: env('AWS_REGION'),
-          endpoint: env('AWS_ENDPOINT'),
-          s3ForcePathStyle: env.bool('AWS_S3_FORCE_PATH_STYLE', true),
+    upload: {
+      config: {
+        provider: '@strapi/provider-upload-aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+            region: env('AWS_REGION'),
+            endpoint: env('AWS_ENDPOINT'),
+            s3ForcePathStyle: env.bool('AWS_S3_FORCE_PATH_STYLE', true),
+          },
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
         },
-        params: {
-          Bucket: env('AWS_BUCKET'),
-        }
-      }
+      },
     },
-  },
-});
+  });
