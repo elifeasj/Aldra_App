3e9f87641f99590ed16562a9527b966fbdcf3950
effy <eliftaydin@gmail.com>
2025-04-25 19:04:39 +0200

    Update index.ts

--- aldra-cms/src/index.ts
+++ aldra-cms/src/index.ts
@@ -1,12 +1,12 @@
 export default {
-  register(/* { strapi } */) {
+  register() {
     console.log('✨ Strapi is registering plugins...');
   },
 
   bootstrap({ strapi }) {
     console.log('🚀 Strapi is bootstrapping...');
 
-    const uploadConfig = strapi.config.get('plugin.upload');
+    const uploadConfig = strapi.config.get('plugin.upload.config');
 
     console.log('✅ Upload config loaded at runtime:', JSON.stringify(uploadConfig, null, 2));
   },
