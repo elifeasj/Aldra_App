87a025155fd28ea49e1eaf8f2bd5badb0763dc7d
effy <eliftaydin@gmail.com>
2025-04-25 16:33:44 +0200

    Update plugins.js

--- aldra-cms/config/plugins.js
+++ aldra-cms/config/plugins.js
@@ -1,16 +1,27 @@
 module.exports = ({ env }) => {
-  console.log('🟢 Strapi loading plugins.js!');
+  const uploadConfig = {
+    provider: '@strapi/provider-upload-aws-s3',
+    providerOptions: {
+      accessKeyId: env('SUPABASE_S3_KEY'),
+      secretAccessKey: env('SUPABASE_S3_SECRET'),
+      region: 'eu-central-1',
+      params: {
+        Bucket: env('SUPABASE_BUCKET'),
+      },
+      endpoint: env('SUPABASE_S3_ENDPOINT'),
+      s3ForcePathStyle: true,
+      signatureVersion: 'v4',
+    },
+  };
+
+  console.log('🟢 plugins.js LOADED!');
+  console.log('📦 Upload provider:', uploadConfig.provider);
+  console.log('📍 S3 Endpoint:', uploadConfig.providerOptions.endpoint);
+  console.log('🪣 Bucket:', uploadConfig.providerOptions.params.Bucket);
 
   return {
     upload: {
-      config: {
-        provider: 'strapi-provider-upload-supabase',
-        providerOptions: {
-          apiUrl: env('SUPABASE_URL'),
-          apiKey: env('SUPABASE_SERVICE_ROLE'),
-          bucket: env('SUPABASE_BUCKET'),
-        },
-      },
+      config: uploadConfig,
     },
   };
 };
