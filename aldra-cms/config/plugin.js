c9643cc5c5e1ee5fe710bf6d05c1051de005e0a9
effy <eliftaydin@gmail.com>
2025-04-25 16:09:07 +0200

    Update plugin.js

--- aldra-cms/config/plugin.js
+++ aldra-cms/config/plugin.js
@@ -1,13 +1,16 @@
-console.log("🟢 Loading Supabase upload plugin");
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-supabase',
-      providerOptions: {
-        apiUrl: env('SUPABASE_URL'),
-        apiKey: env('SUPABASE_SERVICE_ROLE'),
-        bucket: env('SUPABASE_BUCKET'), 
+module.exports = ({ env }) => {
+  console.log('🟢 Strapi loading plugins.js!');
+
+  return {
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-supabase',
+        providerOptions: {
+          apiUrl: env('SUPABASE_URL'),
+          apiKey: env('SUPABASE_SERVICE_ROLE'),
+          bucket: env('SUPABASE_BUCKET'),
+        },
       },
     },
-  },
-});
+  };
+};
