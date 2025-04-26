61dcb0d46774a553c91d30a163b85f5caf9237e0
effy <eliftaydin@gmail.com>
2025-04-26 14:11:02 +0200

    Update guideUtils.ts

--- utils/guideUtils.ts
+++ utils/guideUtils.ts
@@ -3,13 +3,12 @@ import { Guide } from '../types/guides';
 
 export const mapGuideData = (data: any): Guide => ({
   id: data.id,
-  title: data.attributes?.title || data.title || '',
-  slug: data.attributes?.slug || data.slug || '',
-  content: data.attributes?.content || data.content || '',
-  image: data.attributes?.image?.url 
-    ? `${STRAPI_URL}${data.attributes.image.url}` 
-    : (data.image?.url ? `${STRAPI_URL}${data.image.url}` : ''),
-  category: data.attributes?.category || data.category || '',
-  tags: (data.attributes?.tags?.data || data.tags?.data || []).map((tag: any) => tag.attributes.name),
-  help_tags: (data.attributes?.help_tags?.data || data.help_tags?.data || []).map((tag: any) => tag.attributes.name),
+  title: data.title || '',
+  slug: data.slug || '',
+  content: data.content || '',
+  image: data.image?.url ? STRAPI_URL + data.image.url : '',
+  category: data.category || '',
+  tags: data.tags?.data?.map((tag: any) => tag.attributes.name) || [],
+  help_tags: data.help_tags?.data?.map((tag: any) => tag.attributes.name) || [],
 });
+
