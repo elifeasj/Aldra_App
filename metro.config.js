edd75cbfdbf542abe08def859e0f67d23f973d62
effy <eliftaydin@gmail.com>
2025-05-21 20:13:56 +0200

    -

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,14 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  // Add your custom transformer configurations here
+};
+
+config.resolver = {
+  ...config.resolver,
+  // Add your custom resolver configurations here
+};
+
 module.exports = config;
